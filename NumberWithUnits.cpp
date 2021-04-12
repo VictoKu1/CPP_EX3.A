e0319893317c6507aed707d6a27bc489bad9a13b
Victor <kushnir.victor@gmail.com>
2021-04-12 03:33:48 +0300

    .

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -1,6 +1,80 @@
 #include "NumberWithUnits.hpp"
 using namespace std;
 namespace ariel{
-    //*TODO:Write all planned implementation here.
+    NumberWithUnits n(1.0,"Pickles") ;
+    void NumberWithUnits::read_units(ifstream& units_file){
+
+    }
+    int convert (const NumberWithUnits& a,const NumberWithUnits& b){
+        return 0;
+    }
+    NumberWithUnits operator+(const NumberWithUnits& a,const NumberWithUnits& b){
+        return n ;
+    }
+    NumberWithUnits operator+(const NumberWithUnits& a,const double b){
+        return n ;
+    }
+    NumberWithUnits operator+(const double a,const NumberWithUnits& b){
+        return n ;
+    }
+    NumberWithUnits operator+=(const NumberWithUnits& a,const NumberWithUnits& b){
+        return n ;
+    }
+    NumberWithUnits operator-(const NumberWithUnits& a,const NumberWithUnits& b){
+        return n ;
+    }
+    NumberWithUnits operator-(const NumberWithUnits& a,const double b){
+        return n ;
+    }
+    NumberWithUnits operator-(const double a,const NumberWithUnits& b){
+        return n ;
+    }
+    NumberWithUnits operator-(const NumberWithUnits& a){
+        return n ;
+    }
+    NumberWithUnits operator-=(const NumberWithUnits& a,const NumberWithUnits& b){
+        return n ;
+    }
+    bool operator<(const NumberWithUnits& a,const NumberWithUnits& b){
+        return true;
+    }
+    bool operator<=(const NumberWithUnits& a,const NumberWithUnits& b){
+        return true;
+    }
+    bool operator>(const NumberWithUnits& a,const NumberWithUnits& b){
+        return true;
+    }
+    bool operator>=(const NumberWithUnits& a,const NumberWithUnits& b){
+        return true;
+    }
+    bool operator==(const NumberWithUnits& a,const NumberWithUnits& b){
+        return true;
+    }
+    bool operator!=(const NumberWithUnits& a,const NumberWithUnits& b){
+        return true;
+    }
+    NumberWithUnits operator++(const NumberWithUnits& a){
+        return n ;
+    }
+    NumberWithUnits operator++(const NumberWithUnits& a,int){
+        return n ;
+    }
+    NumberWithUnits operator--(const NumberWithUnits& a){
+        return n ;
+    }
+    NumberWithUnits operator--(const NumberWithUnits& a,int){
+        return n ;
+    }
+    NumberWithUnits operator*(const NumberWithUnits& a,const double b){
+        return n ;
+    }
+    NumberWithUnits operator*(const double a,const NumberWithUnits& b){
+        return n ;
+    }
+    ostream& operator<<(ostream& os, const NumberWithUnits& a){
+        return os;
+    }
+    istream& operator>>(istream& is, NumberWithUnits& a){
+        return is;
+    }        
 }
-//*TODO:Remember to include also "working with files" methods .
--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -7,9 +7,38 @@ using namespace std;
 namespace ariel{
     class NumberWithUnits{
         private:
-            //*TODO:Write all planned parameters here.
+            double number;
+            string unit;
         public:
-            //*TODO:Write all planed methods here.
-    }
+            NumberWithUnits(double number,string unit){
+                (*this).number=number;
+                (*this).unit=unit;
+            }
+            static void read_units(ifstream& units_file);
+            friend int convert (const NumberWithUnits& a,const NumberWithUnits& b);
+            friend NumberWithUnits operator+(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend NumberWithUnits operator+(const NumberWithUnits& a,const double b);
+            friend NumberWithUnits operator+(const double a,const NumberWithUnits& b);
+            friend NumberWithUnits operator+=(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend NumberWithUnits operator-(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend NumberWithUnits operator-(const NumberWithUnits& a,const double b);
+            friend NumberWithUnits operator-(const double a,const NumberWithUnits& b);
+            friend NumberWithUnits operator-(const NumberWithUnits& a);
+            friend NumberWithUnits operator-=(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend bool operator<(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend bool operator<=(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend bool operator>(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend bool operator>=(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend bool operator==(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend bool operator!=(const NumberWithUnits& a,const NumberWithUnits& b);
+            friend NumberWithUnits operator++(const NumberWithUnits& a);
+            friend NumberWithUnits operator++(const NumberWithUnits& a,int);
+            friend NumberWithUnits operator--(const NumberWithUnits& a);
+            friend NumberWithUnits operator--(const NumberWithUnits& a,int);
+            friend NumberWithUnits operator*(const NumberWithUnits& a,const double b);
+            friend NumberWithUnits operator*(const double a,const NumberWithUnits& b);
+            friend ostream& operator<<(ostream& os, const NumberWithUnits& a);
+            friend istream& operator>>(istream& is, NumberWithUnits& a);
+    };
 }
 
